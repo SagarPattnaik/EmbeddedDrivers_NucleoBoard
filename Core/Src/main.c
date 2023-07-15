ed449b5e9383fe1730c797bf972df5fd091ce700
pattnasa2013 <sagarpattnaik60@gmail.com>
2023-07-15 15:17:20 -0400

    UART Initialization

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -8,6 +8,10 @@
 #include "main.h"
 
 void SystemClockConfig(void);
+void UART2_Init(void);
+void Error_handler(void);
+
+UART_HandleTypeDef huart2;
 
 int main(void)
 {
@@ -20,4 +24,25 @@ int main(void)
 void SystemClockConfig(void)
 {
   /* By default, the internal RC oscillator is configured to work at 16MHz */
+}
+
+void UART2_Init(void)
+{
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	if ( HAL_UART_Init(&huart2) != HAL_OK )
+	{
+		//There is a problem
+		Error_handler();
+	}
+}
+
+void Error_handler(void)
+{
+	while(1);
 }
--- Core/Src/stm32f4xx_hal_msp.c
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -14,3 +14,29 @@ void HAL_MspInit(void)
 	HAL_NVIC_SetPriority(BusFault_IRQn,0,0);
 	HAL_NVIC_SetPriority(UsageFault_IRQn,0,0);
 }
+
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+  GPIO_InitTypeDef gpio_uart;
+  //here we are going to do the low level inits. of the USART2 peripheral
+  //1. enable the clock for the USART2 peripheral as well as for GPIOA peripheral
+  __HAL_RCC_USART2_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  //2 . Do the pin muxing configurations
+  /**USART2 GPIO Configuration
+  PA2     ------> USART2_TX
+  PA3     ------> USART2_RX
+  */
+  gpio_uart.Pin = GPIO_PIN_2|GPIO_PIN_3;
+  gpio_uart.Mode =GPIO_MODE_AF_PP;
+  gpio_uart.Pull = GPIO_PULLUP;
+  gpio_uart.Speed = GPIO_SPEED_FREQ_LOW;
+  gpio_uart.Alternate =  GPIO_AF7_USART2; //UART2_TX
+  HAL_GPIO_Init(GPIOA,&gpio_uart);
+
+  //3 . Enable the IRQ and set up the priority (NVIC settings )
+  HAL_NVIC_EnableIRQ(USART2_IRQn);
+  HAL_NVIC_SetPriority(USART2_IRQn,15,0);
+
+}
