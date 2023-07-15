83ea59057643d55c31b73c31bf6668c892283198
pattnasa2013 <sagarpattnaik60@gmail.com>
2023-07-15 16:36:38 -0400

    UART_TxRx_Polling

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -10,15 +10,40 @@
 void SystemClockConfig(void);
 void UART2_Init(void);
 void Error_handler(void);
+uint8_t convert_to_capital(uint8_t data);
 
 UART_HandleTypeDef huart2;
+char *user_data = "The application is running\r\n";
 
 int main(void)
 {
-	HAL_Init();
+  HAL_Init();
   SystemClockConfig();
-  while(1);
-	return 0;
+  UART2_Init();
+
+  uint16_t len_of_data = strlen(user_data);
+  HAL_UART_Transmit(&huart2,(uint8_t*)user_data,len_of_data,HAL_MAX_DELAY);
+
+  uint8_t rcvd_data;
+  uint8_t data_buffer[100];
+  uint32_t count=0;
+
+  while(1)
+  {
+    HAL_UART_Receive(&huart2,&rcvd_data,1,HAL_MAX_DELAY);
+    if(rcvd_data == '\r')
+    {
+      break;
+    }
+    else
+    {
+      data_buffer[count++]= convert_to_capital(rcvd_data);
+    }
+  }
+  data_buffer[count++]= '\r';
+  HAL_UART_Transmit(&huart2,data_buffer,count,HAL_MAX_DELAY);
+
+  return 0;
 }
 
 void SystemClockConfig(void)
@@ -40,6 +65,15 @@ void UART2_Init(void)
 		//There is a problem
 		Error_handler();
 	}
+}
+
+uint8_t convert_to_capital(uint8_t data)
+{
+	if( data >= 'a' && data <= 'z')
+	{
+		data = data - ( 'a'- 'A');
+	}
+	return data;
 }
 
 void Error_handler(void)
