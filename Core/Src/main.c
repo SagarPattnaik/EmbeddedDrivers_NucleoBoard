921a7daa99c606b87a5d432abcc2b00f0e24b9a2
pattnasa2013 <sagarpattnaik60@gmail.com>
2023-12-28 16:04:32 -0500

    Current_Measurement_Under_Different_Scenario

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -148,12 +148,12 @@ void GPIO_Init(void)
 void UART2_Init(void)
 {
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
+  huart2.Init.BaudRate = 230400;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
   huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.Mode = UART_MODE_TX;
   if ( HAL_UART_Init(&huart2) != HAL_OK )
   {
     //There is a problem
